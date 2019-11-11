3d9cae35b661f7726902439b3c1f0cb23ba0ea67
likedanny <57621383+likedanny@users.noreply.github.com>
2019-11-11 22:03:50 +0800

    单链表
    关于单链表的一些问题

--- Text.java
+++ Text.java
@@ -1,1 +1,38 @@
-
+//节点类
+class ListNode{
+    public int data;
+    public ListNode next;
+    public ListNode(int date){
+        this.data = data;
+        this.next = null;
+    }
+}
+//标志头
+class SingleList {
+    public ListNode head;
+    public SingleList() {
+        this.head = null;
+    }
+    //打印单链表数据
+    public void diyplay() {
+        if (this.head == null) {
+            return;
+        }
+        ListNode cur = this.head;
+        while (cur != null) {
+            System.out.print(cur.data + " ");
+            cur = cur.next;
+        }
+        System.out.println();
+    }
+    //头插法
+    public void addFirst(int data) {
+        ListNode node = new ListNode(data);
+        if (this.head == null) {
+            this.head = node;
+        } else {
+            node.next = this.head;
+            this.head = node;
+        }
+    }
+}
